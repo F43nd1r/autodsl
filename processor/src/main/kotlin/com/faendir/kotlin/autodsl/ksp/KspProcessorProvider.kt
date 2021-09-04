c4c04981845b1639c5da5a6c85852b62cf495be0
f43nd1r <account+github@faendir.com>
2021-09-04 22:40:26 +0200

    update all the versions

--- processor/src/main/kotlin/com/faendir/kotlin/autodsl/ksp/KspProcessorProvider.kt
+++ processor/src/main/kotlin/com/faendir/kotlin/autodsl/ksp/KspProcessorProvider.kt
@@ -1,12 +1,9 @@
 package com.faendir.kotlin.autodsl.ksp
 
-import com.google.devtools.ksp.processing.CodeGenerator
-import com.google.devtools.ksp.processing.KSPLogger
-import com.google.devtools.ksp.processing.SymbolProcessor
-import com.google.devtools.ksp.processing.SymbolProcessorProvider
+import com.google.devtools.ksp.processing.*
 
 class KspProcessorProvider : SymbolProcessorProvider {
-    override fun create(options: Map<String, String>, kotlinVersion: KotlinVersion, codeGenerator: CodeGenerator, logger: KSPLogger): SymbolProcessor {
-        return KspProcessor(kotlinVersion, codeGenerator, logger)
+    override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
+        return KspProcessor(environment.kotlinVersion, environment.codeGenerator, environment.logger)
     }
 }
