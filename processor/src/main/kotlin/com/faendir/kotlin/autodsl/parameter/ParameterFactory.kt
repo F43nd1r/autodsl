7ee0a102ecdf673c035ea9396a3ef5af31028304
Youssef Shoaib <canonballt@gmail.com>
2026-01-31 22:19:21 +0000

    Implement resolver by delegation (#89)

--- processor/src/main/kotlin/com/faendir/kotlin/autodsl/parameter/ParameterFactory.kt
+++ processor/src/main/kotlin/com/faendir/kotlin/autodsl/parameter/ParameterFactory.kt
@@ -13,64 +13,59 @@ import com.squareup.kotlinpoet.asClassName
 import kotlin.reflect.KClass
 
 class ParameterFactory<A, T : A, C : A, P : A>(
-    private val resolver: SourceInfoResolver<A, T, C, P>,
-) {
+    resolver: SourceInfoResolver<A, T, C, P>,
+) : SourceInfoResolver<A, T, C, P> by resolver {
     private val set = Set::class.asClassName()
     private val list = List::class.asClassName()
     private val collection = Collection::class.asClassName()
     private val iterable = Iterable::class.asClassName()
 
+    @JvmName("getParametersFromConstructor")
     fun getParameters(constructor: C): List<Parameter> =
-        resolver.run {
-            return constructor.getParameters().withIndex().map { (index, parameter) ->
-                val type = parameter.getTypeName()
-                val rawType = type.toRawType()
-                val (hasNestedDsl, collectionType) =
-                    when (rawType) {
-                        set -> {
-                            parameter.hasAnnotatedTypeArgument(AutoDsl::class) to SetType(findSingular(parameter, index))
-                        }
-
-                        list, collection, iterable -> {
-                            parameter.hasAnnotatedTypeArgument(AutoDsl::class) to
-                                ListType(findSingular(parameter, index))
-                        }
-
-                        else -> {
-                            (parameter.getTypeDeclaration()?.hasAnnotation(AutoDsl::class) == true) to null
-                        }
+        constructor.getParameters().withIndex().map { (index, parameter) ->
+            val type = parameter.getTypeName()
+            val rawType = type.toRawType()
+            val (hasNestedDsl, collectionType) =
+                when (rawType) {
+                    set -> {
+                        parameter.hasAnnotatedTypeArgument(AutoDsl::class) to SetType(findSingular(parameter, index))
                     }
-                Parameter(
-                    type,
-                    parameter.getName(),
-                    parameter.getDoc(),
-                    parameter.hasDefault(),
-                    parameter.getAnnotationProperty(AutoDslRequired::class, AutoDslRequired::group),
-                    index,
-                    hasNestedDsl,
-                    collectionType,
-                )
-            }
+
+                    list, collection, iterable -> {
+                        parameter.hasAnnotatedTypeArgument(AutoDsl::class) to
+                            ListType(findSingular(parameter, index))
+                    }
+
+                    else -> {
+                        (parameter.getTypeDeclaration()?.hasAnnotation(AutoDsl::class) == true) to null
+                    }
+                }
+            Parameter(
+                type,
+                parameter.getName(),
+                parameter.getDoc(),
+                parameter.hasDefault(),
+                parameter.getAnnotationProperty(AutoDslRequired::class, AutoDslRequired::group),
+                index,
+                hasNestedDsl,
+                collectionType,
+            )
         }
 
     private fun P.hasAnnotatedTypeArgument(annotation: KClass<out Annotation>): Boolean =
-        resolver.run {
-            return getTypeArguments().firstOrNull()?.hasAnnotation(annotation) ?: false
-        }
+        getTypeArguments().firstOrNull()?.hasAnnotation(annotation) ?: false
 
     private fun findSingular(
         parameter: P,
         index: Int,
     ): String =
-        resolver.run {
-            when {
-                parameter.hasAnnotation(
-                    AutoDslSingular::class,
-                ) -> parameter.getAnnotationProperty(AutoDslSingular::class, AutoDslSingular::value)
+        when {
+            parameter.hasAnnotation(
+                AutoDslSingular::class,
+            ) -> parameter.getAnnotationProperty(AutoDslSingular::class, AutoDslSingular::value)
 
-                parameter.getName().length < 3 -> parameter.getName()
+            parameter.getName().length < 3 -> parameter.getName()
 
-                else -> parameter.getName().singularize(Plurality.CouldBeEither)
-            } ?: "var$index"
-        }
+            else -> parameter.getName().singularize(Plurality.CouldBeEither)
+        } ?: "var$index"
 }
